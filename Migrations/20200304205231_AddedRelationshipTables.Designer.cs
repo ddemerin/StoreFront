3f725ab9f59a587cf33f907043da7a5957cd38e3
Daniel D Demerin <ddemerin@gmail.com>
2020-03-04 15:52:50 -0500

    Added relationship tables

--- Migrations/20200304205231_AddedRelationshipTables.Designer.cs
+++ Migrations/20200304205231_AddedRelationshipTables.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using StoreFront.Models;
@@ -9,9 +10,10 @@ using StoreFront.Models;
 namespace StoreFront.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20200304205231_AddedRelationshipTables")]
+    partial class AddedRelationshipTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
