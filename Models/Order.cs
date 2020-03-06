9abe6ee04935948cb3a81f98abcee9913a8f5f03
Daniel D Demerin <ddemerin@gmail.com>
2020-03-06 11:53:20 -0500

    created controllers for order and itemorders

--- Models/Order.cs
+++ Models/Order.cs
@@ -6,9 +6,8 @@ namespace StoreFront.Models
     public class Order
     {
         public int Id { get; set; }
-        public string Address { get; set; }
-        public string ManagerName { get; set; }
-        public string PhoneNumber { get; set; }
+        public DateTime OrderDate{ get; set; }
+        public string CustomerName { get; set; }
 
         public List<ItemOrder> ItemOrders { get; set; } = new List<ItemOrder>();
     }
